68750d968fc819053552198f36dfacd8d4b9eca9
Peralta-99 <utegenov.erbol2011@mail.ru>
2021-07-12 18:41:02 +0500

    upd baseparser class

--- modules/parser/BaseParser.php
+++ modules/parser/BaseParser.php
@@ -11,19 +11,21 @@ abstract class BaseParser
     protected string $selector;
     protected string $readyToExecuteJs;
     private string $scriptFileName;
+    protected int $neededCountOfArticles;
 
-    public function __construct(string $url, string $selector)
+    public function __construct(string $url, string $selector, int $neededCountOfArticles)
     {
         if (is_executable($this->pathToBinPhantom)) {
             $this->url = $url;
             $this->selector = $selector;
+            $this->neededCountOfArticles = $neededCountOfArticles;
         }
     }
 
     abstract public function executeParse();
 
     public function setVariablesInTemplateJs() {
-        $this->readyToExecuteJs = sprintf($this->getTemplateJsFileOfParse(), $this->url, $this->selector);
+        $this->readyToExecuteJs = sprintf($this->getTemplateJsFileOfParse(), $this->url, $this->selector, $this->neededCountOfArticles);
     }
 
     private function getReadyJs() {
@@ -40,7 +42,7 @@ abstract class BaseParser
     }
 
     private function writeToJsFile() {
-        $hash = md5($this->url) . "_{$this->selector}";
+        $hash = md5($this->url) . "_{$this->selector}_{$this->neededCountOfArticles}";
         $this->scriptFileName = $hash . '.js';
         chdir(__DIR__ . '/js');
         return file_exists($this->scriptFileName) ? true : file_put_contents($this->scriptFileName, $this->getReadyJs());
