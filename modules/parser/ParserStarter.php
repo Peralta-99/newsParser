403ff44c5c198066731d245ea5f9910891ddce7d
Peralta-99 <utegenov.erbol2011@mail.ru>
2021-07-12 14:06:39 +0500

    upd ParserStarter

--- modules/parser/ParserStarter.php
+++ modules/parser/ParserStarter.php
@@ -7,24 +7,29 @@ use PhantomInstaller\PhantomBinary;
 class ParserStarter
 {
     protected string $path;
+    protected string $url;
+    protected string $selector;
 
     /**
-     * @param string $jsFileName add new in modules/parser/js directory
+     * @param string $siteUrl section with articles or news, example: 'https://tjournal.ru/news'
+     * @param string $articleSelector css-class of <a> tag with link of article, example: 'news-feed__item'
+     * @param bool $lazyLoadOfArticles if lazy load articles by onscroll then this argument is true,
+     * else if used pagination - then false
      * @return void
      */
-    public function __construct($jsFileName)
+    public function __construct(string $siteUrl, string $articleSelector, bool $lazyLoadOfArticles = true)
     {
-        $pathToBinPhantom = PhantomBinary::BIN;
-        chdir(__DIR__ . '/js');
-        $dir = getcwd();
-        $pathToExecuteJsFile = "{$pathToBinPhantom} {$dir}/{$jsFileName}";
-        if (is_executable($pathToBinPhantom)) {
-            $this->path = $pathToExecuteJsFile;
-            $this->run();
+        $url = parse_url($siteUrl);
+        $siteUrl = "{$url['scheme']}://{$url['host']}";
+        if ($siteUrl && $articleSelector) {
+            $this->run($lazyLoadOfArticles ?
+                new lazyLoadParser($siteUrl, $articleSelector) :
+                new paginatedParser($siteUrl, $articleSelector));
         }
     }
 
-    protected function run() {
-        exec($this->path);
+    protected function run(BaseParser $parserInstance) {
+        $parserInstance->setVariablesInTemplateJs();
+        $parserInstance->executeParse();
     }
 }
