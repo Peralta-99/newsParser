d43c216322abbd7a424b6011c2175ab2b0af9c91
Peralta-99 <utegenov.erbol2011@mail.ru>
2021-07-16 00:26:58 +0500

    TODO: Implement paginatedParser() class.

---
+++ modules/parser/paginatedParser.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Modules\parser;
+
+
+class paginatedParser extends BaseParser
+{
+    public function executeParse()
+    {
+        // TODO: Implement executeParse() method.
+    }
+
+    protected function writeDataToDb()
+    {
+        // TODO: Implement writeDataToDb() method.
+    }
+
+    protected function getArticleScraperScript()
+    {
+        // TODO: Implement getArticleScraperScript() method.
+    }
+
+    protected function getTemplateJsFileOfParse()
+    {
+        // TODO: Implement getTemplateJsFileOfParse() method.
+    }
+}
