99ba3d8ba810f4a34648cc2b90721411116a663f
Peralta-99 <utegenov.erbol2011@mail.ru>
2021-07-17 01:55:16 +0500

    controller upd

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Http\Request;
 use Modules\parser\ParserStarter;
 
 class ArticlesController extends Controller
@@ -11,5 +12,10 @@ class ArticlesController extends Controller
         $requestData = request()->only(['siteUrl', 'articleSelector', 'countOfArticles', 'lazyLoadOfArticles']);
         $starterInstance = new ParserStarter(...array_values($requestData));
         return response()->json(['jsonFileName' => $starterInstance->fileName]);
+    }
+
+    public function getScrapedArticles(Request $request, $fileName) {
+        return view('articles.index')
+            ->with('articles', Article::where('pulled_from_the_file', $fileName . '.json')->get());
     }
 }
